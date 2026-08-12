2d4a94be1c5662c1345922cfad0b54c2f10458c9
Franco Brunini <valentinobruni@gmail.com>
2026-08-12 15:24:27 -0300

    c

--- ferreira/checkout/checkout.js
+++ ferreira/checkout/checkout.js
@@ -1,5 +1,5 @@
 import { loadHeaderFooter } from "../js/util.mjs";
-import CheckoutProcess from "../js/CheckoutProcess.mjs";
+import CheckoutProcess from "../js/checkoutprocess.mjs";
 
 const myCheckout = new CheckoutProcess("so-cart", "#order-summary");
 
@@ -10,6 +10,7 @@ async function init() {
   });
 
   myCheckout.init();
+  
   
   myCheckout.calculateOrderTotal();
 
